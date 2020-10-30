2fe92fb0826f815cf62deaf4d7f67cdbec90c075
arnut-boonlert <73688555+arnut-boonlert@users.noreply.github.com>
2020-10-30 17:07:24 +0700

    Create website.php

---
+++ website.php
@@ -0,0 +1,17 @@
+  <html>
+  <head>
+    <title>Lab-11</title>
+  </head>
+  <body>
+    <h1>Arnut Boonlert</h1>
+    <h2>Nut</h2>
+    <img src="cat.jpg"/>
+    <img src="shiba.jpg"/>
+    <br />
+    <a href="https://tenor.com/bpa3d.gif">Speechless cat</a>
+    <br />
+    <a href="https://tenor.com/xxS1.gif">Shiba swim</a>
+    <h3>"Doctor, I'm very nervous.  This is the first time I've ever needed an operation"  said the patient.
+The doctor said, "Don't worry, I feel the same.  This is the first operation I've ever performed."</h3>
+  </body>
+</html>
